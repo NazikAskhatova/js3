b675b6fc81a4b1754303a65741300ec1634aa8a9
wiusxt <ashatovanas65@gmail.com>
2021-12-24 14:31:47 +0600

    examples with prompt in js

---
+++ script.js
@@ -0,0 +1,28 @@
+// var message = prompt("What is your message?", "no message");
+// if (message === "") {
+//     message = "no message";
+// }
+// alert(message);
+
+// var year = prompt("When year Kyrgyzstan gained independence?");
+// if (year === "1991") {
+//     alert("Correct!");
+// }
+// else {
+//     alert("Incorrect!")
+// }
+
+// var example1 = "Hello " - "World";
+// console.log(example1);
+
+// var example1 = "100" + 100;
+// // var example1 = "Hello " + "World"
+// var example2 = "10" + "10";
+// console.log(example2);
+
+// // prompt возвращает только строку а не числа
+
+// var age = prompt("How old are u?")
+// var year = 2020 - year;
+// alert(age)
+
